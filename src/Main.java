3f9d51ac94c77b70adffb9bb8acb783b528a0626
crawer <dsdouglas810@gmail.com>
2022-12-12 23:10:46 -0300

    package conflits

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+
+import java.util.Arrays;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Arrays.stream(args)
+                .forEach(System.out::println);
+
+        //Random r = new Random();
+        //System.out.println(r.nextInt(10));
     }
 }
---
+++ src/javabasics/Conflicts.java
@@ -0,0 +1,13 @@
+package javabasics;
+
+import java.util.Date;
+
+public class Conflicts {
+
+    public static void main(String[] args) {
+        //conflicts by package
+        Date date = new Date();
+        java.sql.Date anotherDate = new java.sql.Date(1993L);
+
+    }
+}
