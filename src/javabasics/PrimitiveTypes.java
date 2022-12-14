95c90f5e04b9b75ea516774cb926777e9ea9c7a7
crawer <dsdouglas810@gmail.com>
2022-12-13 21:00:29 -0300

    primitive and Reference types

---
+++ src/javabasics/PrimitiveTypes.java
@@ -0,0 +1,11 @@
+package javabasics;
+
+public class PrimitiveTypes {
+    int num; // 32 bits is allocated
+    long max = 3123456789L;
+
+    int million1 = 1000000; // do not accept null values
+    int million2 = 1_000_000;
+
+
+}
---
+++ src/javabasics/ReferenceTypes.java
@@ -0,0 +1,12 @@
+package javabasics;
+
+import java.util.Date;
+
+public class ReferenceTypes {
+
+    public static void main(String[] args) {
+        Date today = new Date();
+        String greeting = "Douglas";
+    }
+
+}
